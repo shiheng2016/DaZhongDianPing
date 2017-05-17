2b45ef45e63ced336752dab6dcff52395f0fae4d
shiweiping <13120959887@163.com>
2017-05-17 10:48:13 +0800

    React实现大众点评单页面应用

---
+++ mock/home/ad.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    title: '暑假5折',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191639092-2000037796.png',
+    link: 'https://github.com/shiheng2016'
+}, {
+    title: '特价出国',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191648124-298129318.png',
+    link: 'https://github.com/shiheng2016'
+}, {
+    title: '亮亮车',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191653983-1962772127.png',
+    link: 'https://github.com/shiheng2016'
+}, {
+    title: '学钢琴',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191700420-1584459466.png',
+    link: 'https://github.com/shiheng2016'
+}, {
+    title: '电影',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191706733-367929553.png',
+    link: 'https://github.com/shiheng2016'
+}, {
+    title: '旅游热线',
+    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191713186-495002222.png',
+    link: 'https://github.com/shiheng2016'
+}]
